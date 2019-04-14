2e46b8536b96772c236deb55597d65247b449008
TaoQZ <2538474354qq.com>
2019-04-14 20:12:31 +0800

    tao

---
+++ pro_man_system/src/test/GitHubTest.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @Author: zhangjian1
+ * @Date: 2019/4/14 20:08
+ * @Version 1.0
+ */
+public class GitHubTest {
+
+    public void  demo(){
+        System.out.println("dsytz");
+    }
+
+}
